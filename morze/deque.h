478938c7e307a187d9a112fc0666014fb9421a0b
eldardamirov <eldardamirov75@mail.ru>
2018-01-18 12:30:39 +0300

    Morze translator added.

---
+++ morze/deque.h
@@ -0,0 +1,367 @@
+//
+//  deque.h
+//  morze
+//
+//  Created by Эльдар Дамиров on 17.01.2018.
+//  Copyright © 2018 Эльдар Дамиров. All rights reserved.
+//
+
+#ifndef deque_h
+#define deque_h
+
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+
+#define debugMode
+
+enum { NONE, LOW, MEDIUM, FULL };
+
+/*
+ NONE - no secure features at all;
+ LOW - canaries checker;
+ MEDIUM - elements sum, canaries checker;
+ FULL - hash, elements sum, class sum, canaries checker;
+*/
+
+
+//// ------------------------------------------------------------------------------------------------
+const int poisonInt = 12345678;
+const double poisonDouble = NAN;
+const float poisonFloat = NAN;
+const char poisonChar = '~';
+//// ------------------------------------------------------------------------------------------------
+
+
+
+template <typename typeOfData> class Deque
+    {
+    public:
+//        Deque ( size_t bufferSizeTemp, int securityLevelTemp )
+        Deque ( size_t bufferSizeTemp )
+            {
+            bufferSize = bufferSizeTemp;
+            
+            bufferEnd = buffer + ( bufferSize - 1 );
+            
+//            securityLevel = securityLevelTemp;
+            
+            init();
+            
+            }
+            
+        ~Deque()
+            {
+            
+            #ifdef debugMode
+            
+            printf ( "%s", errorList.c_str() );
+            
+            #endif
+            
+            }
+            
+            
+        //////////////////////////////////////////
+        //////////////////////////////////////////
+        //////////////////////////////////////////
+        
+        void test()
+            {
+            for ( int i = 0; i < bufferSize; i++ )
+                {
+                std::cout << buffer [ i ] << "\n";
+                }
+                
+            std::cout << beginDataPointer << "    " << endDataPointer << "\n";
+            }
+        
+        //////////////////////////////////////////
+        //////////////////////////////////////////
+        //////////////////////////////////////////
+            
+        bool push_back ( typeOfData valueToPush )
+            {
+            if ( endDataPointer != beginDataPointer )
+                {
+                *endDataPointer = valueToPush;
+
+                
+                if ( ( endDataPointer + 1 ) > bufferEnd )
+                    {
+                    if ( buffer != beginDataPointer )
+                        {
+                        endDataPointer = buffer;
+                        }
+                    else
+                        {
+                        return false;
+                        }
+                    }
+                else
+                    {
+                    if ( ( endDataPointer + 1 ) != beginDataPointer )
+                        {
+                        endDataPointer++;
+                        }
+                    else
+                        {
+                        return false;
+                        }
+                    }
+                
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+            
+        bool push_front ( typeOfData valueToPush )
+            {
+            if ( beginDataPointer != endDataPointer )
+                {
+                *beginDataPointer = valueToPush;
+                
+                if ( ( beginDataPointer - 1 ) < buffer )
+                    {
+                    if ( bufferEnd != endDataPointer ) 
+                        {
+                        beginDataPointer = bufferEnd;
+                        }
+                    else
+                        {
+                        return false;
+                        }
+                    }
+                else
+                    {
+                    if ( ( beginDataPointer - 1 ) != endDataPointer )
+                        {
+                        beginDataPointer--;
+                        }
+                    else
+                        {
+                        return false;
+                        }
+                    }
+                    
+                    
+                return true;
+                }
+            else 
+                {
+                return false;
+                }
+                
+            }
+            
+            
+            
+        bool pop_back()
+            {
+            
+            #ifdef debugMode
+            
+            *endDataPointer = poisonValue;
+            
+            #endif
+
+            if ( ( endDataPointer - 1 ) != beginDataPointer )
+                {
+                if ( ( endDataPointer - 1 ) < buffer )
+                    {
+                    if ( bufferEnd != beginDataPointer )
+                        {
+                        endDataPointer = bufferEnd;
+                        }
+                    else
+                        {
+                        return false;
+                        }
+                    }
+                else
+                    {
+                    endDataPointer--;
+                    }
+                    
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+            
+        bool pop_front()
+            {
+            
+            #ifdef debugMode
+            
+            *beginDataPointer = poisonValue;
+            
+            #endif
+            
+            
+            if ( ( beginDataPointer + 1 ) != endDataPointer )
+                {
+                if ( ( beginDataPointer + 1 ) > bufferEnd )
+                    {
+                    if ( endDataPointer != buffer )
+                        {
+                        beginDataPointer = buffer;
+                        }
+                    else
+                        {
+                        return false;
+                        }
+                    }
+                else
+                    {
+                    beginDataPointer++;
+                    }
+                    
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+                
+            }
+            
+            
+        typeOfData* front()
+            {
+//            return *beginDataPointer;
+//            typeOfData* elementToReturn = beginDataPointer;
+            
+            if ( ( beginDataPointer + 1 ) != endDataPointer )
+                {
+                if ( ( beginDataPointer + 1 ) > bufferEnd )
+                    {
+                    if ( buffer != endDataPointer )
+                        {
+                        return buffer; 
+                        }
+                    }
+                    
+                return ( beginDataPointer + 1 );
+                }
+
+            return &poisonValue;
+            }
+            
+            
+        typeOfData* back()
+            {
+            
+            if ( ( endDataPointer - 1 ) != beginDataPointer )
+                {
+                if ( ( endDataPointer - 1 ) < buffer )
+                    {
+                    if ( buffer != beginDataPointer )
+                        {
+                        return buffer; 
+                        }
+                    }
+                    
+                return ( endDataPointer - 1 );
+                }
+                
+            return &poisonValue;
+            }
+            
+        size_t size()
+            {
+            return bufferSize;
+            }
+            
+        bool empty()
+            {
+            if ( ( beginDataPointer < endDataPointer ) && ( ( endDataPointer - beginDataPointer ) == 1 ) )
+                {
+                return true;
+                }
+            else
+                {
+                return false;
+                }
+            }
+        
+    
+    
+    private:
+        //// ------------------------------------------------------------------------------------------------
+        typeOfData* buffer = new typeOfData [ bufferSize ] {};
+        typeOfData* bufferEnd = nullptr;
+        typeOfData* beginDataPointer = nullptr;
+        typeOfData* endDataPointer = nullptr;
+    
+        
+        size_t bufferSize = 0;
+        //// ------------------------------------------------------------------------------------------------
+        int securityLevel = NONE;
+        typeOfData poisonValue = NULL;
+        std::string errorList = "";
+        //// ------------------------------------------------------------------------------------------------
+        
+        //// ------------------------------------------------------------------------------------------------
+        
+        //// ------------------------------------------------------------------------------------------------
+        
+    
+    
+        void init()
+            {
+            beginDataPointer = &buffer [ ( ( bufferSize - 1 ) / 2 ) ];
+            endDataPointer = &buffer [ ( ( bufferSize - 1 ) / 2 ) + 1 ]; 
+            
+            #ifdef debugMode
+            setPoisonValue();
+            
+            for ( int currentIndex = 0; currentIndex < bufferSize; currentIndex++ )
+                {
+                buffer [ currentIndex ] = poisonValue;  
+                }
+            #endif
+            
+            }
+
+
+        int setPoisonValue()
+                {
+                if ( typeid ( typeOfData ).name() == typeid ( int ).name() )
+                    {
+                    poisonValue = poisonInt;
+                    }
+                else if ( typeid ( typeOfData ).name() == typeid ( double ).name() )
+                    {
+                    poisonValue = poisonDouble;
+                    }
+                else if ( typeid ( typeOfData ).name() == typeid ( float ).name() )
+                    {
+                    poisonValue = poisonFloat;
+                    }
+                else if ( typeid ( typeOfData ).name() == typeid ( char ).name() )
+                    {
+                    poisonValue = poisonChar;
+                    }
+                else
+                    {
+                    errorList = errorList + "This typename is not supported!\n";
+                    return 1;
+                    }
+                    
+                return 0;
+                }
+    
+    
+    };
+
+
+
+#endif /* deque_h */
--- morze/main.cpp
+++ morze/main.cpp
@@ -7,9 +7,428 @@
 //
 
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <math.h>
+#include <unistd.h>
+#include <thread>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include "deque.h"
+
+enum { silence, dot, dash }; 
+
+
+class morzeEncryptor
+    {
+    public:
+        morzeEncryptor ( std::string voiceTemp ) : messages ( 1024 )
+            {
+            
+            voice = voiceTemp;
+            
+            init();
+            
+            }
+          
+            
+        ~morzeEncryptor()
+            {
+            
+            }
+        
+    
+    
+    private:
+        std::string voice = "";
+        char* commandForDot = "";
+        char* commandForDash = "";
+        
+        int spaceBetweenSignals = 500000; // in milliseconds;
+        
+        Deque <int> messages;
+        
+        void init()
+            {
+            std::string temp  = "say -v " + voice + " 'beep'"; 
+            commandForDot = &temp [ 0 ];
+            
+            temp = "say -v " + voice + " 'beeeeeep'";
+            commandForDash = &temp [ 0 ];
+            
+            controller();
+            }
+        
+        
+        
+        void controller()
+            {
+            char currentChar = ' ';
+            
+            
+            while ( currentChar != '/' )
+                {
+                std::cin >> currentChar;
+                
+                encryptChar ( currentChar );
+                
+                }
+            
+            
+            }
+        
+        
+        int encryptChar ( char currentCharTemp )
+            {
+            char currentChar = ' ';
+            
+            if ( currentCharTemp > 'a' )
+                {
+                currentChar = currentCharTemp - ( 'a' - 'A' );
+                }
+            else    
+                {
+                currentChar = currentCharTemp;
+                }
+            
+            
+            switch ( currentChar )
+            {
+            case 'A':
+                {
+                messages.push_back ( dot );
+                messages.push_back (  silence  );
+                messages.push_back (  dash  );
+                
+                break;
+                }
+            case 'B':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'C':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                    
+                break;
+                }
+            case 'D':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                    
+                break;
+                }
+            case 'E':
+                {
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'F':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'G':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                break;
+                }
+            case 'H':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'I':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'J':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'K':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'L':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'M':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'N':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'O':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'P':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'Q':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'R':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'S':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case 'T':
+                {
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'U':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'V':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence ); 
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'W':
+                {
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'X':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'Y':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                
+                break;
+                }
+            case 'Z':
+                {
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dash );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                messages.push_back ( silence );
+                messages.push_back ( dot );
+                
+                break;
+                }
+            case ' ':
+                {
+                messages.push_back ( silence );
+                messages.push_back ( silence );
+                messages.push_back ( silence );
+                messages.push_back ( silence );
+                
+                break;
+                }
+
+            default:
+                {
+                break;
+                }
+                
+            }
+
+            
+            
+            return 0;
+            }
+        
+        
+        
+        void playSymbol ( int symbol )
+            {
+            
+            if ( symbol == dot )
+                {
+                system ( commandForDot );
+                }
+            
+            if ( symbol == dash )
+                {
+                system ( commandForDash );
+                }
+                
+            if ( symbol == silence )
+                {
+                usleep ( spaceBetweenSignals );
+                } 
+            
+            }
+            
+            
+            
+        
+        
+        
+    
+    
+    };
+
+
+
+int main(int argc, const char * argv[]) 
+    {
+//    system ( "say -v Bells 'beeep'" );
+
+//    char* command = "say -v Bells 'beep'";
+    std::string voice = "Bells";
+    std::string temp  = "say -v " +  voice + " 'beep'"; 
+//            commandForDot = "say -v " + *voice + " 'beep'";
+            char* commandForDot = &temp [ 0 ];
+    
+    system ( commandForDot );
+
+
+    
     return 0;
-}
+    }
